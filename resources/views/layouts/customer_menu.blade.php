c73ea4ee84cd064b62ddd546e0af32ed87b99976
kodir78 <kodir.petani@gmail.com>
2020-07-04 23:11:00 +0800

    Layout Templating

---
+++ resources/views/layouts/customer_menu.blade.php
@@ -0,0 +1,13 @@
+<style>
+    .list-group-item.active {
+        z-index: 2;
+        color: #fff;
+        background-color: #171d26;
+        border-color: #171d26;
+    }
+</style>
+<div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-tachometer-alt"></i> DASHBOARD</a>
+    <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-shopping-cart"></i> MY ORDERS</a>
+    <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-user-circle"></i> MY PROFILE</a>
+</div>
