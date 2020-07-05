f01da24fdaf08521720e8254a68d4caf194a1150
kodir78 <kodir.petani@gmail.com>
2020-07-05 23:40:13 +0800

    Add Helpers Cart

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\CartFacadeServiceProvider::class,
+
 
         /*
          * Application Service Providers...
